a46f8adf2baa17d9e2562f59e8e71c1c875d3ce9
sowkathali01 <sowkathali2k04@gmail.com>
2025-08-05 15:12:55 +0530

    Updated index.html game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$(document).on("click",function() {
   if (!started) {
 
     $("#level-title").text("Level " + level);
