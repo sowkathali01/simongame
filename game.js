9841376db622c1d2a2590d54872a27f3b19bdaea
sowkathali01 <sowkathali2k04@gmail.com>
2025-08-05 15:37:58 +0530

    Fixed bugs commit

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).on("click",function() {
+$(document).keypress(function() {
   if (!started) {
 
     $("#level-title").text("Level " + level);
